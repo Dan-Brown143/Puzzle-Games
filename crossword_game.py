9a05796994c6508f7393ed45c35afe548337ec5f
Dan <danbrown32323@gmail.com>
2026-02-23 01:34:08 +0000

    fixed spelling error

--- crossword_game.py
+++ crossword_game.py
@@ -272,7 +272,7 @@ class CrosswordGame:
     def __init__(self):
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Crossword Puzzle")
-        self.clock = pygame.time.clock()
+        self.Clock = pygame.time.clock()
 
         #Theme
 
@@ -662,7 +662,7 @@ class CrosswordGame:
                 self.draw_complete_screen()
 
             pygame.display.flip()
-            self.clock.tick(60)
+            self.Clock.tick(60)
         
         pygame.quit()
         sys.exit()
